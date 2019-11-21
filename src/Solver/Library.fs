52b7bfde61a26ccfcd16660a91a76db3b317bfb9
James Dorrington <jdorrington@hotmail.com>
2019-11-21 23:14:53 +0000

    Very first attempt at really using F# after 2 hours learning it

---
+++ src/Solver/Library.fs
@@ -0,0 +1,64 @@
+﻿//namespace Solver
+
+//module Say =
+ //   let hello name =
+  //      printfn "Hello %s" name
+module Library
+
+// Get an array of integers for a given row 'i' in a given board 'l'
+let row i (l:list<int>) = [for j in 0..8 -> l.[(i * 9) + j]]
+
+// Get an array of integers for a given column 'i' in a given board 'l'
+let col i (l:list<int>) = [for j in 0..8 -> l.[(j * 9) + i]]
+
+// Get an array of integers for a given cell 'i' in a given board 'l'
+let cell i (l:list<int>) = [for j in [0; 1; 2; 9; 10; 11; 18; 19; 20] -> l.[(((i / 3) * 9) * 3) + ((i % 3) * 3) + j]]
+
+// Get the row number for an individual item
+let getRow i = i / 9
+
+// Get the column number for an individual item
+let getCol i = i % 9
+
+// Get the cell number for an individual item
+// Todo: Fix this into a forumula!!
+let getCell i = match i with
+                | 00 | 01 | 02 | 09 | 10 | 11 | 18 | 19 | 20 -> 0
+                | 03 | 04 | 05 | 12 | 13 | 14 | 21 | 22 | 23 -> 1
+                | 06 | 07 | 08 | 15 | 16 | 17 | 24 | 25 | 26 -> 2
+                | 27 | 28 | 29 | 36 | 37 | 38 | 45 | 46 | 47 -> 3
+                | 30 | 31 | 32 | 39 | 40 | 41 | 48 | 49 | 50 -> 4
+                | 33 | 34 | 35 | 42 | 43 | 44 | 51 | 52 | 53 -> 5                
+                | 54 | 55 | 56 | 63 | 64 | 65 | 72 | 73 | 74 -> 6
+                | 57 | 58 | 59 | 66 | 67 | 68 | 75 | 76 | 77 -> 7
+                | 60 | 61 | 62 | 69 | 70 | 71 | 78 | 79 | 80 -> 8
+                | _ -> -1
+
+// Check to see if list 'l' contains item 'i'
+let rec contains i (l:list<int>) = match l with
+                                   | [] -> false
+                                   | head::tail -> (head = i) || (contains i tail)
+
+// Check to see if list 'l' is 9 elements long and contains full range of 1-9
+// (we can apply this to rows, columns or cells)
+let isComplete (l:list<int>) = (l.Length = 9) && 
+                               [1..9] |> List.forall(fun i -> contains i l)
+
+// Check to see if list 'l' (the board) is complete.
+let isBoardisComplete (l:list<int>) = (l.Length = 9 * 9) &&
+                                      [0..8] |> List.forall(fun i -> isComplete (row i l)) &&
+                                      [0..8] |> List.forall(fun i -> isComplete (col i l)) &&
+                                      [0..8] |> List.forall(fun i -> isComplete (cell i l))
+
+// Get the positions in the board 'l' which currently have the value zero (unassigned)
+let zeroPositions (l:list<int>) = [0..(l.Length-1)] |> List.where(fun i -> (l.[i] = 0))
+
+// Get the list of numbers currently taken for a particular item 'i' in board 'l'
+let getTakenValues i (l:list<int>) =  List.distinct((row (getRow i) l) @ (col (getCol i) l) @ (cell (getCell i) l)) |> List.where(fun i -> i <> 0)
+
+// Get the list of missing numbers for row/column/cell 'l'
+let findMissingNumbers (l:list<int>) = [1..9] |> List.where(fun i -> not (contains i l))
+
+let singleCells (l:list<int>) = zeroPositions l |> List.where(fun i -> ((getTakenValues i l).Length = 8))
+
+let solutions (l:list<int>) = [for i in singleCells l -> (i, findMissingNumbers (getTakenValues i l))]
---
+++ src/Sudoku/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.FSharp.Collections;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        private const int TEXTBOX_WIDTH = 30;
+        private const int TEXTBOX_HEIGHT = 30;
+        private const int BOARD_MARGIN = 50;
+        private const int CELL_MARGIN = 10;
+        private const int SMALL_MARGIN = 5;
+
+        private List<TextBox> InputBoxes = new List<TextBox>();
+
+        public Form1()
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    TextBox textBox = new System.Windows.Forms.TextBox();
+                    textBox.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+                    int xLocation = (x * TEXTBOX_WIDTH) + BOARD_MARGIN + SMALL_MARGIN;
+                    if (x >= 3) xLocation += CELL_MARGIN;
+                    if (x >= 6) xLocation += CELL_MARGIN;
+                    int yLocation = (y * TEXTBOX_HEIGHT) + BOARD_MARGIN + SMALL_MARGIN;
+                    if (y >= 3) yLocation += CELL_MARGIN;
+                    if (y >= 6) yLocation += CELL_MARGIN;
+
+                    textBox.Location = new System.Drawing.Point(xLocation, yLocation);
+                    textBox.Name = "textBox1";
+                    textBox.Size = new System.Drawing.Size(TEXTBOX_WIDTH, TEXTBOX_HEIGHT);
+                    textBox.TabIndex = 0;
+                    textBox.Text = "0";
+                    textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+                    textBox.TextChanged += TextBox_TextChanged;
+                    this.Controls.Add(textBox);
+                    this.InputBoxes.Add(textBox);
+                }
+            }
+
+            //https://www.theguardian.com/lifeandstyle/2019/nov/18/sudoku-4612-easy
+            int[] board =
+            {
+                0, 0, 0,  0, 0, 0,  0, 0, 0,
+                0, 3, 0,  0, 0, 0,  1, 6, 0,
+                0, 6, 7,  0, 3, 5,  0, 0, 4,
+
+                6, 0, 8,  1, 2, 0,  9, 0, 0,
+                0, 9, 0,  0, 8, 0,  0, 3, 0,
+                0, 0, 2,  0, 7, 9,  8, 0, 6,
+
+                8, 0, 0,  6, 9, 0,  3, 5, 0,
+                0, 2, 6,  0, 0, 0,  0, 9, 0,
+                0, 0, 0,  0, 0, 0,  0, 0, 0,
+            };
+
+            InitialiseBoard(board);
+            InitializeComponent();
+
+            this.button1.Focus();
+        }
+        
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!(sender as TextBox).Text.Equals(string.Empty))
+            { 
+                int intVal;
+                if (!int.TryParse(((TextBox) sender).Text, out intVal))
+                {
+                    ((TextBox) sender).Text = string.Empty;
+                }
+                else
+                {
+                    if ((intVal < 1) || (intVal > 9))
+                    {
+                        ((TextBox) sender).Text = string.Empty;
+                    }
+                }
+            }
+        }
+
+        void InitialiseBoard(int[] board)
+        {
+            if ((board.Length != 9 * 9) || (this.InputBoxes.Count != 9 * 9))
+            {
+                return;
+            }
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                this.InputBoxes[i].Text = board[i] == 0 ? " " : board[i].ToString();
+            }
+        }
+
+        int[] CreateArray()
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < this.InputBoxes.Count; i++)
+            {
+                int intVal;
+                if (!int.TryParse(this.InputBoxes[i].Text, out intVal))
+                {
+                    list.Add(0);
+                }
+                else
+                {
+                    list.Add(intVal);
+                }
+            }
+
+            return list.ToArray();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FSharpList<int> list = new FSharpList<int>(CreateArray());
+
+            var foo = Library.solutions(ListModule.OfSeq(CreateArray()));
+            var bar = foo.FirstOrDefault();
+            if (bar != null)
+            {
+                int i1 = bar.Item1;
+                int i2 = bar.Item2.FirstOrDefault();
+                this.InputBoxes[i1].Text = i2.ToString();
+            }
+        }
+    }
+}
